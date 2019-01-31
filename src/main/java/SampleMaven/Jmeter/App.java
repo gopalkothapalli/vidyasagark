d1b583197227d647f76f97289a4fb6222325523c
pavan <pavan1990@gmail.com>
2019-01-31 16:01:48 +0530

    commiting changes

---
+++ src/main/java/SampleMaven/Jmeter/App.java
@@ -0,0 +1,13 @@
+package SampleMaven.Jmeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
